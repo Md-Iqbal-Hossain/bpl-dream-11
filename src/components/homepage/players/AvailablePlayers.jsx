2a568cbf12b99a1fd9832ca247ddeb8d8e976b68
Iqbal <iqbalhossain6615@gmail.com>
2026-03-29 00:38:46 +0600

    button toggling done

--- src/components/homepage/players/AvailablePlayers.jsx
+++ src/components/homepage/players/AvailablePlayers.jsx
@@ -9,7 +9,7 @@ const AvailablePlayers = ({ players }) => {
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
                 {
                     players.map(player => {
-                        console.log(player, 'player');
+                        
 
                         return (
                             <Card player={player} />
--- src/components/homepage/players/Players.jsx
+++ src/components/homepage/players/Players.jsx
@@ -1,15 +1,25 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import AvailablePlayers from './AvailablePlayers';
+import SelectedPlayers from './SelectedPlayers/SelectedPlayers';
 
 const Players = ({playersPromise}) => {
     const players = use(playersPromise);
-    console.log(players);
+    // console.log(players);
+    
+    const [selectedType, setSelectedType] = useState('available');
+    console.log(selectedType, 'selectedType');
     
     return (
-        <div className='container mx-auto'>
-            PLayers: {players.length}
+        <div className='container mx-auto my-[60px]'>
+            <div className='flex justify-between gap-4 items-center mb-[20px]'>
+                {selectedType === 'available' ? <h2 className='font-bold text-3xl'>Available Players</h2> : <h2 className='font-bold text-3xl'>Selected Players (2/6)</h2>}
+                <div>
+                    <button onClick={() => setSelectedType('available')} className={`btn ${selectedType === 'available' ? 'bg-[#E7FE29]' : ''} rounded-r-none rounded-l-xl`}>Available</button>
+                    <button onClick={() => setSelectedType('selected')} className={`btn ${selectedType === 'selected' ? 'bg-[#E7FE29]' : ''} rounded-l-none rounded-r-xl`}>Selected(0)</button>
+                </div>
+            </div>
 
-            <AvailablePlayers players={players}/>
+            {selectedType === 'available' ?<AvailablePlayers players={players}/> : <SelectedPlayers/>}
         </div>
     );
 };
