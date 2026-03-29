af62728557e19775adac83ee6c3fa755ce5dc0bb
Iqbal <iqbalhossain6615@gmail.com>
2026-03-29 15:52:57 +0600

    rendered selected buttons data and functionalities

--- src/components/homepage/players/Players.jsx
+++ src/components/homepage/players/Players.jsx
@@ -14,14 +14,14 @@ const Players = ({playersPromise, setCoin, coin}) => {
     return (
         <div className='container mx-auto my-[60px]'>
             <div className='flex justify-between gap-4 items-center mb-[20px]'>
-                {selectedType === 'available' ? <h2 className='font-bold text-3xl'>Available Players</h2> : <h2 className='font-bold text-3xl'>Selected Players (2/6)</h2>}
+                {selectedType === 'available' ? <h2 className='font-bold text-3xl'>Available Players</h2> : <h2 className='font-bold text-3xl'>Selected Players ({selectedPlayers.length}/{players.length})</h2>}
                 <div>
                     <button onClick={() => setSelectedType('available')} className={`btn ${selectedType === 'available' ? 'bg-[#E7FE29]' : ''} rounded-r-none rounded-l-xl`}>Available</button>
-                    <button onClick={() => setSelectedType('selected')} className={`btn ${selectedType === 'selected' ? 'bg-[#E7FE29]' : ''} rounded-l-none rounded-r-xl`}>Selected(0)</button>
+                    <button onClick={() => setSelectedType('selected')} className={`btn ${selectedType === 'selected' ? 'bg-[#E7FE29]' : ''} rounded-l-none rounded-r-xl`}>Selected({selectedPlayers.length})</button>
                 </div>
             </div>
 
-            {selectedType === 'available' ?<AvailablePlayers players={players} setCoin={setCoin} coin={coin} selectedPlayers={selectedPlayers} setSelectedPlayers={setSelectedPlayers}/> : <SelectedPlayers selectedPlayers={selectedPlayers}/>}
+            {selectedType === 'available' ?<AvailablePlayers players={players} setCoin={setCoin} coin={coin} selectedPlayers={selectedPlayers} setSelectedPlayers={setSelectedPlayers}/> : <SelectedPlayers selectedPlayers={selectedPlayers} setSelectedPlayers={setSelectedPlayers} setCoin={setCoin} coin={coin}/>}
         </div>
     );
 };
--- src/components/homepage/players/SelectedPlayers/SelectedPlayers.jsx
+++ src/components/homepage/players/SelectedPlayers/SelectedPlayers.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
+import { FaRegUser } from 'react-icons/fa';
+import { MdDelete } from 'react-icons/md';
 
-const SelectedPlayers = ({ selectedPlayers }) => {
+const SelectedPlayers = ({ selectedPlayers, setSelectedPlayers, setCoin, coin }) => {
     console.log(selectedPlayers, 'selectedPlayers');
-    
+
+    const handleDeleteSelectedPlayer = (player) => {
+        console.log(selectedPlayers);
+        const filteredPlayers = selectedPlayers.filter(selectedPlayer => selectedPlayer.playerName !== player.playerName);
+        console.log(filteredPlayers);
+        setSelectedPlayers(filteredPlayers);
+        setCoin(coin + player.price);
+    }
+
     return (
         <div>
-            selected players
+            <div className='space-y-5'>
+                {
+                    selectedPlayers.map((player, index) => {
+                        return <div key={index} className='flex items-center gap-6 justify-between p-10 rounded-2xl border'>
+                            <div className='flex items-center gap-6'>
+                                <img src={player.playerImg} alt={player.playerName} className='h-[75px] w-auto rounded-md' />
+                                <div>
+                                    <h2 className='flex items-center gap-2 font-semibold text-xl'><FaRegUser />{player.playerName}</h2>
+                                    <p>{player.playerType}</p>
+                                </div>
+                            </div>
+                            <button className='btn text-red-500' onClick={() => handleDeleteSelectedPlayer(player)}><MdDelete /></button>
+                        </div>
+                    })
+                }
+            </div>
         </div>
     );
 };
